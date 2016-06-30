57db6501c06ad35a6e3812061cfb0cbcbcc19f10
Exiel <zuecortez@gmail.com>
2016-06-29 19:57:04 -0500

    en teoria debe funcionar

--- app/src/main/java/com/example/exiel/mygps/HttpHandler.java
+++ app/src/main/java/com/example/exiel/mygps/HttpHandler.java
@@ -1,47 +1,39 @@
 package com.example.exiel.mygps;
-import java.util.List;
-import java.util.ArrayList;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.message.BasicNameValuePair;
 
+import android.os.AsyncTask;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 /**
  * Created by Exiel on 28/06/2016.
  */
 public class HttpHandler {
+    private RestOperation restOperation = null;
+
+
 
     public String post(String postUrl)
     {
-    MainActivity mainActivity = new MainActivity();
+    MainActivity ma = new MainActivity();
 
         try {
 
-            HttpClient httpclient = new DefaultHttpClient();
+            String latitud_post = String.valueOf(ma.location.getLatitude());
+            String longitud_post = String.valueOf(ma.location.getLongitude());
+            restOperation = new RestOperation();
+            restOperation.execute("http://190.141.120.200:8080/Rutas/ubicacion/guardar/123456?latitud="+latitud_post+"&longitud="+longitud_post);
 
-            HttpPost httpPost = new HttpPost(postUrl + "?latitud=enen&longitud=ned");
-
-
-//            List<NameValuePair> params = new ArrayList<NameValuePair>();
-//
-//            params.add(new BasicNameValuePair("latitud",String.valueOf(mainActivity.location.getLatitude())));
-//
-//            params.add(new BasicNameValuePair("longitud",String.valueOf(mainActivity.location.getLongitude())));
-
-//            httpPost.setEntity(new UrlEncodedFormEntity(params));
-
-            HttpResponse resp = httpclient.execute(httpPost);
-
-            HttpEntity ent = resp.getEntity();
-
-            String text = EntityUtils.toString(ent);
+            String text = restOperation.getStatus().toString();
             return text;
 
         }
@@ -54,7 +46,84 @@ public class HttpHandler {
     }
 
 
+    private class RestOperation extends AsyncTask<String, Void, Void> {
 
+        final HttpClient httpClient = new DefaultHttpClient();
+        String content, error, data;
+        JSONObject jsonResponse;
+        Boolean islogin_message= false;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+
+
+        }
+
+        @Override
+        protected Void  doInBackground(String... params){
+            BufferedReader br = null;
+            URL url;
+
+
+            try {
+                url = new URL(params[0]);
+                URLConnection connection =  url.openConnection();
+                connection.setDoOutput(true);
+                br =  new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuffer sb = new StringBuffer();
+                String line =  null;
+                while ((line = br.readLine()) != null){
+                    sb.append(line);
+                    sb.append(System.getProperty("line.separator"));
+                    content =  sb.toString();
+
+                }
+
+            } catch (MalformedURLException e ){
+                error = e.getMessage();
+                e.printStackTrace();
+            } catch (IOException e) {
+                error = e.getMessage();
+                e.printStackTrace();
+            } finally {
+                /*try {
+                    br.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                */
+                if ((br != null)) {
+                    try {
+                        br.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+            if (error!=null){
+
+            }
+            /*else {
+                try {
+                    jsonResponse =  new JSONObject(content);
+                    //System.out.println("valor del json.. " + jsonResponse);
+                    //String isLoginResponse = jsonResponse.getString("IsLogin");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+             */
+        }
+    }
 
 
 }
